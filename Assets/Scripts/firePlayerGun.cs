af2525826f7b6ad0805293138c2c98b8d4e48474
ThisisJackRyan <thisisjackryan1@gmail.com>
2023-03-14 15:53:46 -0500

    animated Hands

---
+++ Assets/Scripts/firePlayerGun.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class firePlayerGun : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    GameObject gun;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void spawnLaser()
+    {
+        
+    }
+}
